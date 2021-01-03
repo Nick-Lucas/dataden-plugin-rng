1549843e60c25a51b8a32d3f9be6f4c91287aa6e
Nick Lucas <me@nicklucas.co.uk>
2021-01-03 21:20:56 +0000

    Initial Commit

---
+++ dataden-plugin-monzo/README.md
@@ -0,0 +1,17 @@
+# dataden-plugin-monzo
+
+### Setup Instructions
+
+In order to use this plugin in your own DataDen, you will need to create an API application on Monzo and store some credentials in DataDen
+
+#### Create a Monzo API App
+
+* Go to https://developers.monzo.com/ and Sign In, follow the instructions
+* Navigate to the developers portal:
+  * Go to "Clients"
+  * Add a "New OAuth Client"
+    * Name it something sensible
+    * Select "Confidential"
+    * After creation, take a note of the ClientID and Client Secret
+
+####
---
+++ dataden-plugin-monzo/src/index.ts
@@ -0,0 +1,77 @@
+import { createPlugin, Settings } from "@dataden/sdk";
+import * as uuid from 'uuid'
+
+interface PluginSettings {
+  instanceId: string
+}
+
+interface PluginSecrets extends Record<string, string> {
+  testSecret: string
+}
+
+export default createPlugin({
+  getDefaultSettings: async (): Promise<Settings<PluginSettings, PluginSecrets>> => {
+    return {
+      schedule: {
+        every: 1,
+        grain: 'minute'
+      },
+      plugin: {
+        instanceId: 'a'
+      } ,
+      secrets: {
+        testSecret: 'test-value'
+      } 
+    }
+  },
+  loaders: [
+    {
+      name: 'numbers1',
+      load: async (settings, request, log) => {
+        const plugin = settings.plugin as PluginSettings
+        
+        log.info("Data loaded, returning")
+
+        return {
+          lastDate: new Date().toISOString(),
+          mode: 'append',
+          data: [
+            {
+              uniqueId: Date.now,
+              number: Math.trunc((Math.random() * 1000)),
+              randomUuid: uuid.v4(),
+              instance: plugin.instanceId,
+              loader: 1
+            }
+          ]
+        }
+      }
+    },
+    {
+      name: 'numbers2',
+      load: async (settings: Settings<PluginSettings, PluginSecrets>, request, log) => {
+        const plugin = settings.plugin
+
+        if (settings.secrets.testSecret) {
+          log.warn("Credentials expiring soon")
+        } else {
+          throw "Credentials not provided"
+        }
+  
+        return {
+          lastDate: new Date().toISOString(),
+          mode: 'append',
+          data: [
+            {
+              uniqueId: Date.now,
+              number: Math.trunc((Math.random() * 1000)),
+              randomUuid: uuid.v4(),
+              instance: plugin.instanceId,
+              loader: 2
+            }
+          ]
+        }
+      }
+    }
+  ]
+})
