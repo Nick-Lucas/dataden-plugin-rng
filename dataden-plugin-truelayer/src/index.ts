7684d3169e1aab24ba7ce8fd822850401ec45a32
Nick Lucas <me@nicklucas.co.uk>
2021-01-04 23:06:12 +0000

    Add frame for OAuth2 solution

--- dataden-plugin-truelayer/src/index.ts
+++ dataden-plugin-truelayer/src/index.ts
@@ -1,4 +1,4 @@
-import { createPlugin, Settings } from "@dataden/sdk";
+import { createPlugin, Settings, PluginAuth } from "@dataden/sdk";
 import * as uuid from 'uuid'
 import axios from "axios";
 
@@ -9,27 +9,38 @@ interface PluginSettings {
 interface PluginSecrets extends Record<string, string> {
   truelayerClientId: string
   truelayerClientSecret: string
-  bankCode: string
-}
-
-async function getAccessToken(secrets: PluginSecrets) {
-  try {
-    const response = await axios.post("https://auth.truelayer.com/connect/token", {
-      client_id: secrets.truelayerClientId,
-      client_secret: secrets.truelayerClientSecret,
-      code: secrets.bankCode,
-      grant_type: "authorization_code",
-      redirect_uri: "https://console.truelayer.com/redirect-page",
-    })
-    return response.data
-  } catch (e) {
-    return e.response.status + ": "+ JSON.stringify(e.response.data, null, 2)
-  }
 }
 
 // const api = axios.create({
 //   baseURL: "https://auth.truelayer.com/connect/token"
 // })
+
+interface Tokens {
+  access_token: string
+  expires_in: string
+  token_type: string
+  refresh_token: string
+}
+
+const exchangeForTokens = async (
+  settings: Settings<PluginSettings, PluginSecrets>, 
+  receivedParams: 
+    { grant_type: "authorization_code", code: string } | 
+    { grant_type: "refresh_token", refresh_token: string }
+  ): Promise<Tokens> => {
+  try {
+    const response = await axios.post("https://auth.truelayer.com/connect/token", {
+      client_id: settings.secrets.truelayerClientId,
+      client_secret: settings.secrets.truelayerClientSecret,
+      ...receivedParams,
+      redirect_uri: "https://console.truelayer.com/redirect-page",
+    })
+    
+    return response.data as Tokens
+  } catch (e) {
+    throw e.response.status + ": "+ JSON.stringify(e.response.data, null, 2)
+  }
+}
 
 export default createPlugin({
   getDefaultSettings: async (): Promise<Settings<PluginSettings, PluginSecrets>> => {
@@ -44,10 +55,52 @@ export default createPlugin({
       secrets: {
         truelayerClientId: "",
         truelayerClientSecret: "",
-        bankCode: ""
       } 
     }
   },
+
+  authMethod: {
+    type: "oauth2_authorizationcode",
+    getAuthUri: async (settings: Settings<PluginSettings, PluginSecrets>, params) => {
+      // TODO: what about reauthorization later?
+      return axios.getUri({
+        baseURL: "https://auth.truelayer.com/",
+        params: {
+          response_type: "code",
+          client_id: settings.secrets.truelayerClientId,
+          redirect_uri: params.redirectUri,
+          state: params.state,
+        }
+      })
+    },
+    exchangeAuthorizationForAuthState: async (
+      settings: Settings<PluginSettings, PluginSecrets>, 
+      receivedParams: { code: string }
+    ) => {
+      return exchangeForTokens(settings, {
+        grant_type: "authorization_code",
+        code: receivedParams.code
+      })
+    },
+    updateAuthState: async (
+      settings: Settings<PluginSettings, PluginSecrets>, 
+      previousTokens: Tokens
+    ) => {  
+      try {
+        return exchangeForTokens(settings, {
+          grant_type: "refresh_token",
+          refresh_token: previousTokens.refresh_token
+        })
+      } catch (e) {
+        if (String(e).includes("403")) {
+          return "reauthorization_required"
+        } else {
+          throw e
+        }
+      }
+    }
+  } as PluginAuth.OAuth2AuthMethod,
+
   loaders: [
     {
       name: 'transactions',
@@ -55,10 +108,7 @@ export default createPlugin({
         if (!settings.secrets.bankCode || !settings.secrets.truelayerClientId || !settings.secrets.truelayerClientSecret) {
           throw "Secrets not populated"
         }
-        const accessResponse = await getAccessToken(settings.secrets)
-
-        log.info("TOKEN RESPONSE\n", accessResponse)
-
+        
         return {
           lastDate: new Date().toISOString(),
           mode: 'append',
