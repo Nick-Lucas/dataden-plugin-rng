bcc7c27398f4d1c7927f2efda95248611321d1f4
Nick Lucas <me@nicklucas.co.uk>
2021-05-15 17:04:13 +0100

    Add tests and support date types in generateBatches

--- plugin-truelayer/src/generateBatches.test.ts
+++ plugin-truelayer/src/generateBatches.test.ts
@@ -1,3 +1,73 @@
+import { generateBatches, Batch } from "./generateBatches";
+
 describe("generateBatches", () => {
-  
+  it('works on strings', () => {
+    const batches = generateBatches(
+      "2020-01-01T00:00:00Z",
+      "2020-06-01T00:00:00Z",
+      2
+    )
+
+    expect(batches).toEqual([
+      {
+        dateFromISO: "2020-01-01T00:00:00.000Z",
+        dateToISO: "2020-03-01T00:00:00.000Z",
+        failCount: 0
+      },
+      {
+        dateFromISO: "2020-03-01T00:00:00.000Z",
+        dateToISO: "2020-05-01T00:00:00.000Z",
+        failCount: 0
+      },
+      {
+        dateFromISO: "2020-05-01T00:00:00.000Z",
+        dateToISO: "2020-06-01T00:00:00.000Z",
+        failCount: 0
+      },
+    ] as Batch[])
+  })
+
+  it('works on dates', () => {
+    const batches = generateBatches(
+      new Date("2020-01-01T00:00:00Z"),
+      new Date("2020-06-01T00:00:00Z"),
+      2
+    )
+
+    expect(batches).toEqual([
+      {
+        dateFromISO: "2020-01-01T00:00:00.000Z",
+        dateToISO: "2020-03-01T00:00:00.000Z",
+        failCount: 0
+      },
+      {
+        dateFromISO: "2020-03-01T00:00:00.000Z",
+        dateToISO: "2020-05-01T00:00:00.000Z",
+        failCount: 0
+      },
+      {
+        dateFromISO: "2020-05-01T00:00:00.000Z",
+        dateToISO: "2020-06-01T00:00:00.000Z",
+        failCount: 0
+      },
+    ] as Batch[])
+  })
+
+  it('throws on invalid inputs', () => {
+    expect(() => {
+      generateBatches(
+        "kjnasdkjnad",
+        new Date("2020-01-01T00:00:00Z"),
+        2
+      )
+    }).toThrowError(/Could not parse 'left' .*/)
+    
+    expect(() => {
+      generateBatches(
+        new Date("2020-01-01T00:00:00Z"),
+        "asjinasdjnasd",
+        2
+      )
+    }).toThrowError(/Could not parse 'toDate' .*/)
+  })
 })
--- plugin-truelayer/src/generateBatches.ts
+++ plugin-truelayer/src/generateBatches.ts
@@ -5,15 +5,30 @@ export interface Batch {
   dateToISO: string;
   failCount: number;
 }
-export function generateBatches(fromDateISO: string, toDateISO: string, batchLengthMonths: number): Batch[] {
+
+export function generateBatches(fromDateISO: string | Date, toDateISO: string | Date, batchLengthMonths: number): Batch[] {
   const batches: Batch[] = [];
 
   const grain = Duration.fromObject({
     months: batchLengthMonths
   });
 
-  let left = DateTime.fromISO(fromDateISO);
-  const toDate = DateTime.fromISO(toDateISO);
+  if (fromDateISO instanceof Date) {
+    fromDateISO = fromDateISO.toISOString()
+  }
+  if (toDateISO instanceof Date) {
+    toDateISO = toDateISO.toISOString()
+  }
+
+  let left = DateTime.fromISO(fromDateISO).toUTC();
+  const toDate = DateTime.fromISO(toDateISO).toUTC();
+
+  if (!left.isValid) {
+    throw new Error("Could not parse 'left' date when generating batches:" + left.invalidExplanation)
+  }
+  if (!toDate.isValid) {
+    throw new Error("Could not parse 'toDate' date when generating batches:" + toDate.invalidExplanation)
+  }
 
   while (left.valueOf() < toDate.valueOf()) {
     const right = DateTime.min(left.plus(grain), toDate);
